8c4248c079130a9b61b9c3ae048ee3049322e57a
srinivasanramaraju <srinivasanramaraju@users.noreply.github.com>
2017-06-22 16:29:42 -0500

    Merging to master (#1)
    * intial push
    
    * Adding instructions and removing extra spaces
    
    * removing extra spaces
    
    * updating readme instructions

---
+++ customread.py
@@ -0,0 +1,66 @@
+from fileparser import Fileparser
+from collections import defaultdict
+import time
+
+# Function to find key with most unique values and to compute average
+def simpleComputation(uniqueDict,type=0):
+    max_key=""
+    max_val=0
+    if type == 1:
+        avgDuration = defaultdict(list)
+        for k, v in uniqueDict.items():
+            avgDuration[k] = (1.0 * sum(v)) / len(v)
+        return avgDuration
+    else:
+        for k,v in uniqueDict.items():
+            if len(v) > max_val:
+                max_key = k
+                max_val = len(v)
+        return max_key
+
+# Main Driver Function
+def main():
+    logFile1 = Fileparser("log_201612_01.csv")
+    logFile2 = Fileparser("log_201612_02.csv")
+    logFile1Lst=logFile1.readAllLines()
+    logFile2Lst=logFile2.readAllLines()
+    logFile1Lst.extend(logFile2Lst[1:])
+    uniqueNetizens=defaultdict(set)
+    avgTimeSpent=defaultdict(list)
+    popularDate =defaultdict(set)
+    popularSection=defaultdict(set)
+    for eachitem in logFile1Lst[1:]:
+        eachitem = eachitem.split(",")
+        date=eachitem[0]
+        userId=eachitem[1]
+        country=eachitem[2]
+        webAddress=eachitem[3]
+        duration=eachitem[-1]
+        if 'www.' in webAddress:
+            section = webAddress.split("/")[1]
+            if userId != '':
+                popularSection[section].add(userId)
+            avgTimeSpent[webAddress].append(int(duration) if duration !='' else 0)
+        if country != '' and userId != '' and country != "unknown":
+            uniqueNetizens[country].add(userId)
+        try:
+            time.strptime(date, '%m/%d/%y')
+            if userId != '':
+                 popularDate[date].add(userId)
+        except ValueError:
+            continue
+    maxCountry = simpleComputation(uniqueNetizens)
+    print("a. From which country did the site receive the most unique visitors?")
+    print(maxCountry)
+    avgDuration = simpleComputation(avgTimeSpent,1)
+    print("b. What was the average time spent visiting a page?")
+    print(avgDuration)
+    popularDay = simpleComputation(popularDate)
+    print("c. What was the most popular day to visit the site (as calculated by the number of unique visitors)? What day of the week was that date? ")
+    print (time.strftime("%A", time.strptime(popularDay, '%m/%d/%y')))
+    popular_section = simpleComputation(popularSection)
+    print("d. What is the most popular section of the webiste (as calculated by the number of unique visitors)?")
+    print(popular_section)
+
+if __name__ == '__main__':
+    main()
---
+++ fileparser.py
@@ -0,0 +1,37 @@
+class Fileparser():
+    '''
+        File Parser Implementation.
+    '''
+    def __init__(self,fileName):
+             self.logFile = open(fileName)
+             self.fileName = fileName
+             self.delimiter = ','
+
+    # Function to read line by line from log file
+    def readLine(self):
+         logFileLine = self.logFile.readline().strip()
+         tmp = logFileLine
+         while logFileLine == "" or tmp.replace(self.delimiter,'') == '':
+             logFileLine=self.logFile.readline()
+             if logFileLine == '':
+                 break
+             else:
+                 logFileLine = logFileLine.strip()
+                 tmp = logFileLine
+         if logFileLine != "":
+             return logFileLine
+         else:
+             return None
+
+    # Function to read all lines of the log file
+    def readAllLines(self, type=0):
+        if type ==0:
+            logFile = Fileparser(self.fileName)
+            eachLine = logFile.readLine()
+            allLines=[]
+            while eachLine is not None:
+                allLines.append(eachLine)
+                eachLine = logFile.readLine()
+            return allLines
+        else:
+            return open(self.fileName)
